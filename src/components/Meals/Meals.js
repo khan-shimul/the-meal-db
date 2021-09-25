8dff4b4b304a5128b8810d020e1e93304eaa6bca
shimulkhan <khanshimul196@gmail.com>
2021-09-25 21:08:21 +0600

    search emplement

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -4,6 +4,7 @@ import Cart from '../Cart/Cart';
 
 const Meals = () => {
     const [meals_A, setMeals_A] = useState([])
+    const [displayMeal, setDisplayMeal] = useState([])
     const [cart, setCart] = useState([])
     // console.log(cart)
 
@@ -11,7 +12,10 @@ const Meals = () => {
     useEffect(() => {
         fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
             .then(res => res.json())
-            .then(data => setMeals_A(data.meals))
+            .then(data => {
+                setMeals_A(data.meals)
+                setDisplayMeal(data.meals)
+            })
     }, [])
 
     const handleAddCart = product => {
@@ -20,13 +24,27 @@ const Meals = () => {
         // console.log(cart)
     }
 
+    const handleSearch = event => {
+        const searchText = event.target.value
+        const mathcedMeals = meals_A.filter(meal => meal.strMeal.toLowerCase().includes(searchText.toLowerCase()))
+        // console.log(mathcedMeals.length)
+
+        setDisplayMeal(mathcedMeals);
+
+    }
+
     return (
         <div className="overflow-hidden container">
+            <div class="input-group my-3">
+                <input onChange={handleSearch} type="text" class="form-control" placeholder="Fish" aria-label="Recipient's username" aria-describedby="button-addon2" />
+                <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+            </div>
+
             <div className="row">
                 <div className="col-md-9">
                     <div className="row">
                         {
-                            meals_A.map(meal_A => <Ameal
+                            displayMeal.map(meal_A => <Ameal
                                 mealA={meal_A}
                                 handleAddCart={handleAddCart}
                                 key={meal_A.idMeal}
